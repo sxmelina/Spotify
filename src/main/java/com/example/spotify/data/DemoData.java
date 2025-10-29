f12816b8d99f3ae3979981219a318dca2954c401
Melina Salijević <148011168+sxmelina@users.noreply.github.com>
2025-10-29 15:21:16 +0100

    Ispisivanje zeljenih albuma i pjesama

--- src/main/java/com/example/spotify/data/DemoData.java
+++ src/main/java/com/example/spotify/data/DemoData.java
@@ -15,33 +15,24 @@ public class DemoData {
 
     public DemoData() {
         // Kreiranje albuma
-        albumi.add(new Album(1L, "The Dark Side of the Moon", "Progressive Rock", 1973, "Pink Floyd"));
+        albumi.add(new Album(1L, "Sredinom", "New age", 2000, "Dino Merlin"));
         albumi.add(new Album(2L, "Thriller", "Pop", 1982, "Michael Jackson"));
-        albumi.add(new Album(3L, "Back in Black", "Hard Rock", 1980, "AC/DC"));
-        albumi.add(new Album(4L, "Abbey Road", "Rock", 1969, "The Beatles"));
+        albumi.add(new Album(3L, "Pakt sa đavolom", "Rep", 2017, "Jala & Buba Corelli"));
+        albumi.add(new Album(4L, "1989", "Pop", 2014, "Taylor Swift"));
 
-        // Kreiranje pjesama
-        // Pink Floyd - The Dark Side of the Moon
-        pjesme.add(new Pjesma(1L, "Speak to Me", "1:13", 1973, 1L));
-        pjesme.add(new Pjesma(2L, "Breathe", "2:43", 1973, 1L));
-        pjesme.add(new Pjesma(3L, "Time", "6:53", 1973, 1L));
-        pjesme.add(new Pjesma(4L, "Money", "6:23", 1973, 1L));
-
+       //dino
+        pjesme.add(new Pjesma(1L, "Godinama", "3:56", 2000, 1L));
+        pjesme.add(new Pjesma(2L, "Halima", "4:16", 2000, 1L));
         // Michael Jackson - Thriller
         pjesme.add(new Pjesma(5L, "Wanna Be Startin' Somethin'", "6:03", 1982, 2L));
         pjesme.add(new Pjesma(6L, "Thriller", "5:57", 1982, 2L));
-        pjesme.add(new Pjesma(7L, "Beat It", "4:18", 1982, 2L));
-        pjesme.add(new Pjesma(8L, "Billie Jean", "4:54", 1982, 2L));
+        //jala
+        pjesme.add(new Pjesma(3L, "22", "4:15", 2017, 3L));
+        pjesme.add(new Pjesma(4L, "Advokat", "4:32", 2017, 3L));
+        // Taylor
+        pjesme.add(new Pjesma(7L, "Wonderland", "4:06", 2014, 4L));
+        pjesme.add(new Pjesma(8L, "New Romantics", "3:51", 2014, 4L));
 
-        // AC/DC - Back in Black
-        pjesme.add(new Pjesma(9L, "Hells Bells", "5:12", 1980, 3L));
-        pjesme.add(new Pjesma(10L, "Back in Black", "4:15", 1980, 3L));
-        pjesme.add(new Pjesma(11L, "You Shook Me All Night Long", "3:30", 1980, 3L));
-
-        // The Beatles - Abbey Road
-        pjesme.add(new Pjesma(12L, "Come Together", "4:20", 1969, 4L));
-        pjesme.add(new Pjesma(13L, "Something", "3:03", 1969, 4L));
-        pjesme.add(new Pjesma(14L, "Here Comes the Sun", "3:05", 1969, 4L));
     }
 
     public List<Album> getAlbumi() {
